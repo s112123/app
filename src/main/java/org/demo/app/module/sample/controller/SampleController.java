5d063d4b532ee6cd44fd4a782a6fc3a6079ec31f
s112123 <s112123@naver.com>
2025-09-01 17:16:36 +0900

    Commit 3

--- src/main/java/org/demo/app/module/sample/controller/SampleController.java
+++ src/main/java/org/demo/app/module/sample/controller/SampleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
-    @GetMapping("/")
+    @GetMapping("/app")
     ResponseEntity<?> sample() {
         return ResponseEntity.ok("Hello Jenkins");
     }
