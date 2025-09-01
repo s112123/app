7f825f952b2a025b6492f574f9f38cb65c6f9580
s112123 <s112123@naver.com>
2025-09-01 15:25:23 +0900

    Commit 1

---
+++ src/main/java/org/demo/app/module/sample/controller/SampleController.java
@@ -0,0 +1,14 @@
+package org.demo.app.module.sample.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/")
+    ResponseEntity<?> sample() {
+        return ResponseEntity.ok("Hello Jenkins");
+    }
+}
